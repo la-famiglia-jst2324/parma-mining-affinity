1e05990d31556f80862b7cc983755617529b7e1f
Mehmet Fatih Teloğlu <43525350+mfteloglu@users.noreply.github.com>
2023-11-27 22:40:41 +0100

    feat: Setup client to fetch organizations (#5)
    Co-authored-by: Simon Okutan <90206664+Nomiez@users.noreply.github.com>

--- parma_mining/affinity/api/main.py
+++ parma_mining/affinity/api/main.py
@@ -1,6 +1,17 @@
 """Main entrypoint for the API routes in of parma-analytics."""
 
 from fastapi import FastAPI
+from typing import List
+from parma_mining.affinity.model import OrganizationModel
+from parma_mining.affinity.client import AffinityClient
+from dotenv import load_dotenv
+from starlette import status
+import os
+
+load_dotenv()
+
+base_url = str(os.getenv("AFFINITY_BASE_URL") or "")
+api_key = str(os.getenv("AFFINITY_API_KEY") or "")
 
 app = FastAPI()
 
@@ -10,3 +21,11 @@ app = FastAPI()
 def root():
     """Root endpoint for the API."""
     return {"welcome": "at parma-mining-affinity"}
+
+
+@app.get("/organizations", status_code=status.HTTP_200_OK)
+def get_all_organizations() -> List[OrganizationModel]:
+    """Fetch all tracked companies from Affiniy CRM."""
+
+    _affinityCrawler = AffinityClient(api_key, base_url)
+    return _affinityCrawler.collect_companies()
