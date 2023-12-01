3d5934508b9fda3f302c1a930f7f776ebddf1243
mfteloglu <mfteloglu@gmail.com>
2023-12-01 17:01:11 +0100

    feat: implement paging

--- parma_mining/affinity/client.py
+++ parma_mining/affinity/client.py
@@ -11,29 +11,33 @@ class AffinityClient:
         self.api_key = api_key
         self.base_url = base_url
 
-    def get(self, path: str) -> Response:
+    def get(self, path: str, params: dict[str, str]) -> Response:
         return requests.get(
             self.base_url + path,
             auth=HTTPBasicAuth("", self.api_key),
             headers={"Content-Type": "application/json"},
+            params=params,
         )
 
     def collect_companies(self) -> List[OrganizationModel]:
         path = "/organizations"
-        response = self.get(path)
+        response = self.get(path, {"": ""}).json()
         organizations = []
 
-        for result in response.json()["organizations"]:
-            parsed_organization = OrganizationModel.model_validate(
-                {
-                    "id": result["id"],
-                    "name": result["name"],
-                    "domain": result["domain"] or "",
-                    "domains": result["domains"],
-                    "crunchbase_uuid": result["crunchbase_uuid"] or "",
-                }
-            )
-
-            organizations.append(parsed_organization)
+        while response["next_page_token"]:
+            for result in response["organizations"]:
+                parsed_organization = OrganizationModel.model_validate(
+                    {
+                        "id": result["id"],
+                        "name": result["name"],
+                        "domain": result["domain"] or "",
+                        "domains": result["domains"],
+                        "crunchbase_uuid": result["crunchbase_uuid"] or "",
+                    }
+                )
+                organizations.append(parsed_organization)
+            response = self.get(
+                path, params={"page_token": response["next_page_token"]}
+            ).json()
 
         return organizations
